6a6588ad61731bb2b29122a79e3d043abae777c6
Punit-art <68048936+Punit-art@users.noreply.github.com>
2020-12-16 21:02:45 +0530

    Update ps.sh

--- ps.sh
+++ ps.sh
@@ -4,4 +4,4 @@ sleep 0.1
 echo -e "\033[1;32mSorry Username This tool is under construction 🏗️👷👷👷👷🚧👷👷\033[0m"
 echo -e "\033[1;32mTool Will be very soon \033[0m"
 figlet Bye| lolcat
-echo "                                                                        Byunit "
+echo "                                                                        By Punit "
